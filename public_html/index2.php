7c152eaf5144219bc807b1403e61769232581bb5
vasagor <londoneyepad@hotmail.com>
2023-07-21 18:14:00 +0200

    another commit

---
+++ public_html/index2.php
@@ -0,0 +1,15 @@
+<?php
+define('CONSTANTE', 5);
+echo CONSTANTE;
+echo '<br/>';
+
+echo 'Current PHP version: ' . PHP_VERSION;
+echo '<br/>';
+
+$stringSample = 'valor';
+var_dump($stringSample);
+echo '<br/>';
+
+$intSample = 2;
+var_dump($intSample);
+echo '<br/>';
